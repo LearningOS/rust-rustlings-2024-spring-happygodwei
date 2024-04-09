a3bf3fc6afa62e500568b04d87ea6bf1eb661290
happygodwei <90402141+happygodwei@users.noreply.github.com>
2024-04-09 14:40:17 +0000

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -27,13 +26,20 @@ struct Team {
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
-
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
+
+        let v = scores.entry(team_1_name).or_insert(Team{goals_scored:0,goals_conceded:0});
+        v.goals_scored+=team_1_score;
+        v.goals_conceded+=team_2_score;
+        let v2 = scores.entry(team_2_name).or_insert(Team{goals_scored:0,goals_conceded:0});
+        v2.goals_scored+=team_2_score;
+        v2.goals_conceded+=team_1_score;
+        
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be the number of goals conceded from team_2, and similarly
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,22 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let res=String::from(input);
+    String::from(res.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let res=String::from(input);
+    res+" world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]

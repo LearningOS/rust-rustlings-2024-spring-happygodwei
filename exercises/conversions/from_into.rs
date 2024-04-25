4a45f3cbee0c4e1bc6fdb54bbfdbe0660840b975
happygodwei <90402141+happygodwei@users.noreply.github.com>
2024-04-25 15:04:06 +0000

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len()==0 {
+            Default::default()
+        }else{
+            let ss :Vec<&str> = s.split(",").collect();
+            if ss[0].len()==0{
+                Default::default()
+            }else if ss.len()==1 || ss[1].len()==0 || ss.len()>2{
+                Default::default()
+            }else {
+                if let Ok(num) = ss[1].parse::<usize>(){
+                    Person{name:ss[0].to_string(),age: num } 
+                }else{
+                    Default::default()
+                }            
+            }
+            
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len()==0 {
+            Err(ParsePersonError::Empty)
+        }else{
+            let ss :Vec<&str> = s.split(",").collect();
+
+            if ss.len()==1{
+                Err(ParsePersonError::BadLen)
+            }else if  ss[0].len()==0{
+                Err(ParsePersonError::NoName)
+            }else if  ss.len()>2{
+                Err(ParsePersonError::BadLen)
+            }else {
+                match ss[1].parse::<usize>(){
+                    Ok(num) => Ok(Person{name:ss[0].to_string(),age: num }) ,
+                    Err(num) =>Err(ParsePersonError::ParseInt(num)) ,
+                }
+                    
+            }
+            
+        }
     }
 }
 
